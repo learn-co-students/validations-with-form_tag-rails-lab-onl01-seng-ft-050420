08b745b0189d501a8f0922dc46eef8ab9f0a8b0b
qu1xs <kevinnealmurphy@gmail.com>
2020-07-03 22:19:23 -0400

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,9 @@
 class Author < ActiveRecord::Base
+    # name is not blank
+    # email is unique
+    # phone_number is exactly 10 digits long
+
+    validates :name, presence: true
+    validates :email, uniqueness: true
+    validates :phone_number, length: { is: 10 }
 end
