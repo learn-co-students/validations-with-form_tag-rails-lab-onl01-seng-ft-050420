e8591d61b7cf2fa57e0aea44237875edf3ffd072
uharston <uharston@gmail.com>
2020-07-13 12:58:25 -0500

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,6 +1,9 @@
 class AuthorsController < ApplicationController
+  before_action :set_author, only: [:show, :edit, :update]
+
+
   def show
-    @author = Author.find(params[:id])
+    # @author = Author.find(params[:id])
   end
 
   def new
@@ -8,12 +11,28 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    @author = Author.create(author_params)
-
-    redirect_to author_path(@author)
+    @author = Author.new(author_params)
+    if @author.valid? 
+      @author.save
+      redirect_to author_path(@author)
+    else 
+      render :new 
+    end 
   end
 
+  def edit 
+
+  end 
+
+  def update 
+
+  end 
+
   private
+
+  def set_author 
+    @author = Author.find(params[:id])
+  end 
 
   def author_params
     params.permit(:name, :email, :phone_number)
--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,6 @@
 class Author < ActiveRecord::Base
+    validates :name, presence: true 
+    validates :name, :email, uniqueness: true 
+    validates :phone_number, length: { is: 10 }
+    validates :phone_number, format: { with: /\A\d+\z/}
 end
